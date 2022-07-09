8820abf9ef091bad97569644f019d97d7ba2dd63
jatinkh25 <jatinkhatri25.jk@gmail.com>
2022-07-09 11:48:55 +0530

    drawing options images added

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,4 +1,9 @@
 import React, { useRef, useEffect, useState } from "react";
+import { ReactComponent as Eraser } from "../../assets/eraser.svg";
+import { ReactComponent as Pen } from "../../assets/pen.svg";
+import { ReactComponent as Rectangle } from "../../assets/rectangle.svg";
+import { ReactComponent as Selection } from "../../assets/selection.svg";
+import { ReactComponent as Line } from "../../assets/line.svg";
 import "./styles.css";
 
 //https://iconscout.com/icons/
@@ -7,13 +12,9 @@ export default function Canvas() {
   const contextRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [selectedMode, setSelectedMode] = useState("pen");
-  const [strokeWidth, setStrokeWidth] = useState(4);
-
-  const toolbarOptions = [
-    { id: "pen", value: "Pen" },
-    { id: "eraser", value: "Erase" },
-    { id: "select", value: "Select" },
-  ];
+  const [strokeWidth] = useState(4);
+  // const [penClick, setPenClick] = useState(false);
+  const [iconsClickedState, setIconsClickedState] = useState([true, false * 4]);
 
   const penSizes = [];
   for (let i = 2; i <= 20; ++i) {
@@ -22,7 +23,6 @@ export default function Canvas() {
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    context.strokeStyle = "salmon";
     context.lineWidth = strokeWidth;
     contextRef.current = context;
   }, [strokeWidth]);
@@ -67,24 +67,27 @@ export default function Canvas() {
       clientX - boundingRect.left,
       clientY - boundingRect.top
     );
+    contextRef.current.lineCap = "round";
     contextRef.current.stroke();
   };
 
-  const toolbarComp = toolbarOptions.map((option) => {
-    return (
-      <span key={option.id} onClick={() => setSelectedMode(option.id)}>
-        {option.value}
-      </span>
-    );
-  });
+  const changeIconStateHandler = (idx) => {
+    const iconStateArr = Array(iconsClickedState.length).fill(false);
+    iconStateArr[idx] = true;
+    setIconsClickedState(iconStateArr);
+    if (idx === 0) {
+      setSelectedMode("pen");
+    } else if (idx === 1) {
+      setSelectedMode("eraser");
+    } else if (idx === 1) {
+      setSelectedMode("line");
+    } else if (idx === 1) {
+      setSelectedMode("rect");
+    } else {
+      setSelectedMode("select");
+    }
+  };
 
-  const strokeSizesComp = penSizes.map((option) => {
-    return (
-      <option key={option} value={option}>
-        {option}
-      </option>
-    );
-  });
   return (
     <>
       <canvas
@@ -94,16 +97,61 @@ export default function Canvas() {
         onMouseMove={draw}
         width={window.innerWidth * 0.98}
         height={window.innerHeight * 0.95}
-        style={{ strokeLinecap: "round" }}
       ></canvas>
+
+      {/* credits @images https://www.onlinewebfonts.com/icon */}
       <div className="options_container">
-        {toolbarComp}
-        <select
-          value={strokeWidth}
-          onChange={(e) => setStrokeWidth(e.target.value)}
+        <div
+          className={
+            iconsClickedState[0] ? "svg_icon pen click" : "svg_icon pen"
+          }
+          onClick={() => changeIconStateHandler(0)}
         >
-          {strokeSizesComp}
-        </select>
+          <Pen
+            stroke={iconsClickedState[0] ? "#fff" : null}
+            fill={iconsClickedState[0] ? "#fff" : "#000"}
+          />
+        </div>
+        <div
+          className={
+            iconsClickedState[1] ? "svg_icon eraser click" : "svg_icon eraser"
+          }
+          onClick={() => changeIconStateHandler(1)}
+        >
+          <Eraser stroke={iconsClickedState[1] ? "#fff" : null} />
+        </div>
+        <div
+          className={
+            iconsClickedState[2] ? "svg_icon line click" : "svg_icon line"
+          }
+          onClick={() => changeIconStateHandler(2)}
+        >
+          <Line fill={iconsClickedState[2] ? "#fff" : "#000"} />
+        </div>
+        <div
+          className={
+            iconsClickedState[3] ? "svg_icon rect click" : "svg_icon rect"
+          }
+          onClick={() => changeIconStateHandler(3)}
+        >
+          <Rectangle
+            stroke={iconsClickedState[3] ? "#fff" : "#000"}
+            strokeWidth="15px"
+            fill={iconsClickedState[3] ? "#fff" : "#000"}
+          />
+        </div>
+        <div
+          className={
+            iconsClickedState[4] ? "svg_icon select click" : "svg_icon select"
+          }
+          onClick={() => changeIconStateHandler(4)}
+        >
+          <Selection
+            color={iconsClickedState[4] ? "#fff" : "#000"}
+            stroke={iconsClickedState[4] ? "#fff" : null}
+            fill={iconsClickedState[4] ? "#fff" : "#000"}
+          />
+        </div>
       </div>
     </>
   );
