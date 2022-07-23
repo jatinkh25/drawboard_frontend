b682802e5c2441dbb4920d94fce9ce83d4d8eb98
jatinkh25 <jatinkhatri25.jk@gmail.com>
2022-07-23 10:32:09 +0530

    changed select-icon svg, background opacity removed

--- src/components/svg/selection.js
+++ src/components/svg/selection.js
@@ -4,9 +4,12 @@ export const Selection = ({ color, stroke, fill }) => {
 			xmlns='http://www.w3.org/2000/svg'
 			xmlnsXlink='http://www.w3.org/1999/xlink'
 			viewBox='0 0 16 16'
+			color={color}
+			stroke={stroke}
+			fill={fill}
 		>
 			<defs>
-				<path color={color} stroke={stroke} fill={fill} id='a' d='M0,0h2v2h-2Z' />
+				<path id='a' d='M0,0h2v2h-2Z' />
 			</defs>
 			<use transform='translate(3.75 12)' xlinkHref='#a' />
 			<path d='M0,1c0,0.552246 0.447754,1 1,1h1v-2h-2v1Z' transform='translate(-.25 12)' />
